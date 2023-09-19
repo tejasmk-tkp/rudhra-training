5cb6cd1facfb8e27c4e0b88ea9d54d862c923c8c
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-20 00:06:01 +0530

    added code to measure keyboard press time

--- day3/velocity.py
+++ day3/velocity.py
@@ -1,4 +1,24 @@
 from scipy.integrate import quad
+import keyboard
+import time
+
+w_pressed = False
+start_time = 0
+
+while True:
+
+    if keyboard.is_pressed("w"):
+        if not w_pressed:
+            w_pressed = True
+            start_time = time.time()
+    else:
+        if w_pressed:
+            w_pressed = False
+            end_time = time.time()
+            T = end_time - start_time
+            print(f"Time = {T:.2f}")
+
+    time.sleep(0.01)
 
 def v(T):
     def g(t,k,n):
