cec8a95894e38e1dc9af1dbab3c88b15689535e4
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-19 13:23:41 +0530

    Fixed loop issue and updated code as a function

--- day4/velocity.c
+++ day4/velocity.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #define N 1000
-float vel(int rate, ) {
-	float t, v, a;
 
-	printf("Enter time in seconds: ");
-	scanf("%d", &t);
+float vel(int rate, int time ) {
+	float v = 0, a = 0;
 	
-	for (float i = 0; i <=  t; i += t/N) {
-		a = 4*t*t;
-		v += a * (t/N);
+	for (int i = 0; i <= N; i++) {
+		float time = (float) i/N;
+		a = rate*time*time;
+		v += a * (time/(float)N);
 	}
-
-	printf("v = %f", v);
+	return v;
 
 }
+
+int main() {return 0;}
