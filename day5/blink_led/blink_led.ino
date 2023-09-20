526c8e3224d36618e4ad7d04f20dea57cab6c16b
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-20 12:39:01 +0530

    Arduino LED code update

--- day5/blink_led/blink_led.ino
+++ day5/blink_led/blink_led.ino
@@ -1,31 +1,36 @@
 const int led = 13;
+int ledState = LOW;
+int key_press = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
+  digitalWrite(led, ledState);
   Serial.println("Press 1 to turn on LED and 0 to turn off");
+}
+void command() {
+  if (Serial.available() == 0) {
+
+    key_press = Serial.parseInt();
+
+    switch (key_press) {
+      case 0:
+        ledState = LOW;
+        break;
+      case 1:
+        ledState = HIGH;
+        break;
+      default:
+        Serial.println("Enter only 1 or 0");
+        break;
+    }
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available() == 0) {
-
-    int key_press = Serial.parseInt();
-
-    if (key_press == 1) {
-        digitalWrite(led, HIGH);
-        key_press = Serial.parseInt();
-        if (key_press == 0) {
-          digitalWrite(led, LOW);
-          key_press = Serial.parseInt();
-          if (key_press == 1) {
-            digitalWrite(led, HIGH);
-            break;
-          }
-        }
-      }
-    }
-  }
+    command();
+    digitalWrite(led, ledState);
+}
 
