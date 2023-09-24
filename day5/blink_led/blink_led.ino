a147160949da67e2686a31215645ed14d9765506
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-24 11:58:42 +0530

    fixed the code to receive inputs continously

--- day5/blink_led/blink_led.ino
+++ day5/blink_led/blink_led.ino
@@ -1,36 +1,24 @@
 const int led = 13;
 int ledState = LOW;
-int key_press = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   digitalWrite(led, ledState);
-  Serial.println("Press 1 to turn on LED and 0 to turn off");
+  Serial.println("Press 1");
 }
-void command() {
-  if (Serial.available() == 0) {
+void loop() {
+  if (Serial.available()) {
+    int key_press = Serial.parseInt();
 
-    key_press = Serial.parseInt();
-
-    switch (key_press) {
-      case 0:
-        ledState = LOW;
-        break;
-      case 1:
-        ledState = HIGH;
-        break;
-      default:
-        Serial.println("Enter only 1 or 0");
-        break;
+    if (key_press == 1 && ledState == LOW) {
+      ledState = HIGH;
     }
+
+    else if (key_press == 1 && ledState == HIGH) {
+      ledState = LOW;
+    }
+    digitalWrite(led, ledState);
   }
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-    command();
-    digitalWrite(led, ledState);
-}
-
