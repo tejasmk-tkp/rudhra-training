4a02a3b30f0c136e378a0330b5dfe94d17552c4a
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-26 18:37:09 +0530

    fixed code

--- day8/DC_motor_direction_control/DC_motor_direction_control.ino
+++ day8/DC_motor_direction_control/DC_motor_direction_control.ino
@@ -3,7 +3,7 @@ const int IN2 = 7;
 
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   digitalWrite(IN1, LOW);
@@ -14,18 +14,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   //Direction 1
   if (Serial.available()) {
-        
+
     int key = Serial.parseInt();
     
     if (key == 1) {
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, HIGH);
-      }
+    }
   
     if (key == 2) {
       digitalWrite(IN1, HIGH);
       digitalWrite(IN2, LOW);
-      }
+    }
 
     if (key == 0) {
       digitalWrite(IN1, LOW);
