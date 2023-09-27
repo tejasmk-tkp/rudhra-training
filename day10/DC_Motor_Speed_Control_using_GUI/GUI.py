99818cd3f9797e86bd0a2d94491975414553f536
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-27 22:52:47 +0530

    updated code to send data to arduino

--- day10/DC_Motor_Speed_Control_using_GUI/GUI.py
+++ day10/DC_Motor_Speed_Control_using_GUI/GUI.py
@@ -1,14 +1,16 @@
 from tkinter import *
+import serial as sl
 
 root = Tk()
 root.title("DC Motor Speed and Direction Control")
 
+arduino = sl.Serial("/dev/ttyACM0", 9600)
 PWM = IntVar()
 
 label = Label(root, text="Speed")
 label.pack(pady = 10)
 
-slider = Scale(root, from_ = 0, to = 255, variable = PWM, orient = "vertical", length = 300, sliderlength = 40, command = lambda val: (label.config(text = f"PWM: {val}"), print(f"PWM: {val}")))
+slider = Scale(root, from_ = 0, to = 255, variable = PWM, orient = "vertical", length = 300, sliderlength = 40, command = lambda PWM_val: (label.config(text = f"PWM: {PWM_val}"), arduino.write(str(PWM_val).encode())))
 slider.pack()
 
 root.mainloop()
