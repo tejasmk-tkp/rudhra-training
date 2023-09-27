f5d1edad66ae1dcddcf62b65523794431e39d199
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-27 17:06:00 +0530

    included code to get PWM values using potentiometer

--- day9/speed_control_dc_motor/speed_control_dc_motor.ino
+++ day9/speed_control_dc_motor/speed_control_dc_motor.ino
@@ -1,12 +1,15 @@
 const int IN1 = 6;
 const int IN2 = 7;
 const int PWM = 5;
+const int POT = A0;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+  pinMode(POT, INPUT);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   analogWrite(PWM, 255);
@@ -18,7 +21,8 @@ void loop() {
   if (Serial.available()) {
 
     int key = Serial.read();
-    int PWM_val = Serial.parseInt();
+    int PWM_val = analogRead(POT);
+    PWM_val = map(PWM_val, 403, 1023, 255, 0);
 
     if (key == '1') {
       digitalWrite(IN1, LOW);
