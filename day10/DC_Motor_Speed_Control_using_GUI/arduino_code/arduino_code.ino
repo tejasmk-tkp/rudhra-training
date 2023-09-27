333a31a6795242c8f9537e6c3df3339921b526d8
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-27 22:53:32 +0530

    Arduino code to receive and print the data received from the GUI

---
+++ day10/DC_Motor_Speed_Control_using_GUI/arduino_code/arduino_code.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    int data = Serial.parseInt();
+    Serial.print("Received: ");
+    Serial.println(data);
+  }
+}
