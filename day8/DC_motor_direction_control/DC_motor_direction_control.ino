40905f9cbd811a5134dbe652da8a1f2061ce84e8
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-27 17:04:46 +0530

    fixed code issues

--- day8/DC_motor_direction_control/DC_motor_direction_control.ino
+++ day8/DC_motor_direction_control/DC_motor_direction_control.ino
@@ -1,11 +1,15 @@
 const int IN1 = 6;
 const int IN2 = 7;
+const int ENA = 5;
+const int POT = A0;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
+  pinMode(POT, INPUT);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
 }
@@ -16,20 +20,25 @@ void loop() {
   if (Serial.available()) {
 
     int key = Serial.parseInt();
+    int PWM = analogRead(POT);
+    PWM = map(PWM, 403, 1023, 0, 255);
     
     if (key == 1) {
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, HIGH);
+      analogWrite(ENA, PWM);
     }
   
     if (key == 2) {
       digitalWrite(IN1, HIGH);
       digitalWrite(IN2, LOW);
+      analogWrite(ENA, PWM);
     }
 
     if (key == 0) {
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, LOW);
+      analogWrite(ENA, PWM);
     }
   }
 }
