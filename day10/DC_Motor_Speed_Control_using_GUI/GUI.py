faabaf116cb26ef1c35829f5f2a6acbfa051eb87
tejasmk-tkp <dawn3tkp@gmail.com>
2023-09-29 15:11:10 +0530

    Updated arduino code to run the motor based on input from GUI

--- day10/DC_Motor_Speed_Control_using_GUI/GUI.py
+++ day10/DC_Motor_Speed_Control_using_GUI/GUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 import serial as sl
 import struct
 
-arduino = sl.Serial("/dev/ttyACM0", 9600)
+arduino = sl.Serial("/dev/ttyACM1", 9600)
 
 def send_PWM(value):
     value = int(value)
@@ -14,11 +14,16 @@ root.title("DC Motor Speed and Direction Control")
 
 PWM = IntVar()
 
-label = Label(root, text="Speed")
-label.pack(pady = 10)
+PWM_label = Label(root, text="Speed")
+PWM_label.pack(pady = 10)
 
 slider = Scale(root, from_ = 0, to = 255, variable = PWM, orient = "vertical", length = 300, sliderlength = 40, command = send_PWM)
 
 slider.pack()
+
+dir_label = Label(root, text="Direction")
+dir_label.pack(pady = 30)
+
+
 
 root.mainloop()
--- day10/DC_Motor_Speed_Control_using_GUI/arduino_code/arduino_code.ino
+++ day10/DC_Motor_Speed_Control_using_GUI/arduino_code/arduino_code.ino
@@ -1,13 +1,46 @@
+const int IN1 = 6;
+const int IN2 = 7;
+const int PWM = 5;
+//const int POT = A0;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  //pinMode(POT, INPUT);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  //analogWrite(PWM, 255);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    int data = Serial.read();
-    Serial.print("Received: ");
-    Serial.println(data);
+  //Direction 1
+  if (Serial.available()) {
+
+    int PWM_val = Serial.read();
+    Serial.println(PWM_val);
+    //int PWM_val = analogRead(POT);
+    //PWM_val = map(PWM_val, 403, 1023, 255, 0);
+
+    //if (key == '1') {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(PWM, PWM_val);
+    //}
+  
+    // if (key == '2') {
+    //   digitalWrite(IN1, HIGH);
+    //   digitalWrite(IN2, LOW);
+    //   analogWrite(PWM, PWM_val);
+    // }
+
+    // if (key == '0') {
+    //   digitalWrite(IN1, LOW);
+    //   digitalWrite(IN2, LOW);
+    //   analogWrite(PWM, PWM_val);
+    // }
   }
 }
