5648bd0ea7419552c8b0a8e7d35726c7fec3333c
tejasmk-tkp <dawn3tkp@gmail.com>
2023-10-09 14:12:11 +0530

    updated code to fix few errors

--- holiday_task/angle_correction.py
+++ holiday_task/angle_correction.py
@@ -1,69 +1,43 @@
 import random as rn
-#from time import *
-import math
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
-class PIDController:
-    def __init__(self, kp, ki, kd, setpoint):
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
-        self.setpoint = setpoint
-        self.error = 0
-        self.integral = 0
-        self.derivative = 0
-        self.error_prev = 0
+'''class PID:
+    def __init__(self, Kp, Ki, Kd):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.time = 0
+        self.time_prev = -1e-6
+        self.I = 0
+        self.e_prev = 0'''
 
-    def update(self, process_variable):
-        self.error = setpoint - process_variable
-        self.integral += self.error
-        self.derivative = (self.error - self.error_prev)
-        output = self.kp*self.error + self.ki*self.integral + self.kd*self.derivative
-        self.error_prev = self.error
-        return output
+def PID(Kp, Ki, Kd, setpoint, feedback):
+    
+    time = 0
+    time_prev = -1e-6
+    I = 0
+    e_prev = 0
+
+    e = setpoint - feedback
+    print(e)    
+    P = Kp*e
+    I += Ki*e*(time - time_prev)
+    D = Kd*(e - e_prev)/(time - time_prev)
+    print(P, I, D)
+    Output = P + I + D
+
+    e_prev = e
+    time_prev = time
+
+    return Output
 
 noise = rn.gauss(0, 15)
 setpoint = int(input("Enter Target Angle: "))
-processVariable = setpoint + noise
-#setpoint = 10
+feedback = setpoint + noise
+print(setpoint, feedback)
 
-pid_controller = PIDController(1, 0.1, 0.01, setpoint)
+output = PID(0.1, 0.01, 0, setpoint, feedback)
+print(output)
 
-for t in range(100):
-    measured_value = processVariable
-    output = pid_controller.update(measured_value)
-    error = pid_controller.error
-    print(setpoint, measured_value, error, output)
-
-plt.plot(measured_value)
-plt.plot(output)
-#plt.plot(setpoint)
-plt.show()
-
-'''Kp, Ki, Kd = 1, 0, 0
-
-e = sp - pv
-
-print(sp, pv, e)
-
-prev_e = 0
-integral = 0
-
-while pv != sp:
-
-    P = Kp*e
-
-    integral += e
-    I = Ki*integral
-
-    D = Kd*(e-prev_e)
-    prev_e = e
-
-    pv = P + I + D
-
-    e = sp - pv
-
-    print(sp, pv, e)
-
-    sleep(1)
-'''
