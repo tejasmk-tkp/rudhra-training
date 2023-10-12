3d15bf04b7b4514c0fb918011da6610ad96e1b87
tejasmk-tkp <dawn3tkp@gmail.com>
2023-10-12 22:46:12 +0530

    Tuning

--- PID_Controller/angle_correction.py
+++ PID_Controller/angle_correction.py
@@ -46,7 +46,7 @@ plt.grid()
 
 while feedback != setpoint:
     noise = rn.gauss(0, 15)
-    output = PID(2, 2, 0, setpoint, feedback)
+    output = PID(1.5, 128, 0, setpoint, feedback)
     #print(output)
     feedback += output + noise
     #print(feedback)
