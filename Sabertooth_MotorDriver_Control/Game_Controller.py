f2ff1a94da7ffc8405ba4c4caf67e539d8818ceb
tejasmk-tkp <dawn3tkp@gmail.com>
2023-10-18 18:49:53 +0530

    mapped all the values

--- Sabertooth_MotorDriver_Control/Game_Controller.py
+++ Sabertooth_MotorDriver_Control/Game_Controller.py
@@ -3,7 +3,7 @@ import sys
 import time
 import serial as sl
 
-#motor_driver = sl.serial("/dev/ttyUSB0")
+motor_driver = sl.serial("/dev/ttyUSB0", 9600)
 
 def controller():
     pygame.init()
@@ -25,12 +25,12 @@ def controller():
                     sys.exit()
 
             left_stick = (controller.get_axis(0), controller.get_axis(1))
-            #right_stick = (controller.get_axis(2), controller.get_axis(3))
+            right_stick = (controller.get_axis(2), controller.get_axis(3))
 
             #print(f"Left: {left_stick}")
             #print(f"Right: {right_stick}")
 
-            return left_stick
+            return (left_stick, right_stick)
 
     except KeyboardInterrupt:
         pygame.quit()
@@ -46,24 +46,32 @@ def map_range(value, from_low, from_high, to_low, to_high):
 
 while True:
     controlValue = controller()
-    #print(controlValue[0])
+    left = controlValue[0]
+    right = controlValue[1]
+    #print(left)
 
-    '''#Forward and Backward (1, -1)
-    M1_forward = map_range(controlValue[1], 1, -1, 1, 127)
-    M2_forward = map_range(controlValue[1], 1, -1, 128, 255)
-    f_data = bytes([M1_forward, M2_forward])
-    print(controlValue[1], M1_forward, M2_forward, f_data)
-    #motor_driver.write(f_data)'''
+    #Conflict
+    '''if left[1] != 0 and right[1] != 0:
+        M1_forward = map_range(left[1], 1, -1, 1, 127)
+        M2_forward = map_range(left[1], 1, -1, 128, 255)
+        f_data = bytes([M1_forward, M2_forward])
+        print(left[1], M1_forward, M2_forward, f_data)
+        #motor_driver.write(f_data)'''
+    
+    #Forward and Backward (1, -1)
+    if left[1] != 0:
+        M1_forward = map_range(left[1], 1, -1, 1, 127)
+        M2_forward = map_range(left[1], 1, -1, 128, 255)
+        f_data = bytes([M1_forward, M2_forward])
+        print(left[1], M1_forward, M2_forward, f_data)
+        motor_driver.write(f_data)
 
-    #Left (-1, 0)
-    M1_turn = map_range(controlValue[0], 0, -1, 64, 127) #Left Motor
-    M2_turn = map_range(controlValue[0], 0, -1, 192, 255) #Right Motor
-    t_data = bytes([M1_turn, M2_turn])
-    print(controlValue[0], M1_turn, M2_turn, t_data)
-
-
-    '''#Right (0, 1)
-    M1_backward = map_range(controlValue[1], 0, 1, , ) #Left Motor
-    M2_backward = map_range(controlValue[1], 0, 1, , ) #Right Motor'''
+    #Left and Right(-1, 1)
+    if right[1] != 0:
+        M1_turn = map_range(right[1], 1, -1, 1, 127) #Left Motor
+        M2_turn = map_range(right[1], -1, 1, 128, 255) #Right Motor
+        t_data = bytes([M1_turn, M2_turn])
+        print(right[1], M1_turn, M2_turn, t_data)
+        motor_driver.write(f_data)
 
     time.sleep(0.1)
