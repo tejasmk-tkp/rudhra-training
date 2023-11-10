1e777603224f8dd39327f6a2eb8f2f8af1cacf0f
tejasmk-tkp <tejasmk.tkp@gmail.com>
2023-11-10 14:13:48 +0530

    fixed direction control issue

--- Arduino/DC_Motor_Speed_Control_using_GUI/GUI.py
+++ Arduino/DC_Motor_Speed_Control_using_GUI/GUI.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import serial as sl
 
-arduino = sl.Serial("/dev/ttyACM0", 9600)
+arduino = sl.Serial("/dev/ttyUSB0", 9600)
 
 def send_PWM(value):
     value = bytes([int(value)])
@@ -15,7 +15,7 @@ PWM = IntVar()
 PWM_label = Label(root, text="Speed and Direction Slider")
 PWM_label.pack(pady = 10)
 
-slider = Scale(root, from_ = -255, to = 255, variable = PWM, orient = "vertical", length = 510, sliderlength = 40, command = send_PWM)
+slider = Scale(root, from_ = 0, to = 255, variable = PWM, orient = "vertical", length = 510, sliderlength = 40, command = send_PWM)
 
 slider.pack()
 
--- Arduino/DC_Motor_Speed_Control_using_GUI/arduino_code/arduino_code.ino
+++ Arduino/DC_Motor_Speed_Control_using_GUI/arduino_code/arduino_code.ino
@@ -1,6 +1,6 @@
-const int IN1 = 6;
-const int IN2 = 7;
-const int PWM = 5;
+const int IN1 = 4;
+const int IN2 = 5;
+const int PWM = 6;
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,17 +18,20 @@ void loop() {
 
     byte PWM_val = Serial.read();
     Serial.println(PWM_val);
+    
+    int PWM_val_R = map(PWM_val, 1, 128, 255, 0);
+    int PWM_val_L = map(PWM_val, 129, 255, 0, 255);
 
-    if (PWM_val > 0) {
+    if (PWM_val > 0 && PWM_val < 129) {
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, HIGH);
-      analogWrite(PWM, PWM_val);
+      analogWrite(PWM, PWM_val_R);
     }
 
-    if (PWM_val < 0) {
+    if (PWM_val > 128 && PWM_val < 256) {
       digitalWrite(IN1, HIGH);
       digitalWrite(IN2, LOW);
-      analogWrite(PWM, PWM_val);
+      analogWrite(PWM, PWM_val_L);
     }
 
     if (PWM_val == 0) {
